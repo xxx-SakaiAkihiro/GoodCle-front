8f9bb1880e22bfd511c47fd8f1caf04a58a3c32c
ikedariho <riho.ikeda@rakus-partners.co.jp>
2020-07-10 12:17:29 +0900

    apiを取得

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const READ_TMDbAPI = "READ_TMDbAPI";
-
-const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1";
-const QUERYSTRING = "?token=token123";
+//api.themoviedb.org/3/movie/550?api_key=
+const ROOT_URL = "https://api.themoviedb.org/3/discover/movie?";
+const APIKEY = "api_key=5ba450d095bb065f3e8766d5877e86eb";
+const QUERYSTRING = "sort_by=popularity.desc";
 
 export const readTMDbAPI = () => async (dispatch) => {
-  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`);
+  const response = await axios.get(`${ROOT_URL}${APIKEY}&${QUERYSTRING}`);
   console.log(response);
   dispatch({ type: READ_TMDbAPI, response });
 };
