a9a12c17f728e5555769167b02f72f2cb69d370b
ikedariho <riho.ikeda@rakus-partners.co.jp>
2020-07-10 13:24:44 +0900

    apikeyを環境変数に設定

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,13 +4,10 @@ export const READ_TMDbAPI = "READ_TMDbAPI";
 const ROOT_URL = "https://api.themoviedb.org/3/discover/movie?";
 const QUERYSTRING = "sort_by=popularity.desc";
 
-const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const readTMDbAPI = () => async (dispatch) => {
-  console.log("API" + REACT_APP_API_KEY);
-  const response = await axios.get(
-    `${ROOT_URL}${process.env.REACT_APP_API_KEY}&${QUERYSTRING}`
-  );
+  const response = await axios.get(`${ROOT_URL}${API_KEY}&${QUERYSTRING}`);
   console.log(response);
   dispatch({ type: READ_TMDbAPI, response });
 };
